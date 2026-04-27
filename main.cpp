7a9a6e72f7565248c702c9a2f9f917aade80cf7d
XillisWu <xillis@outlook.com>
2026-04-27 18:16:05 +0800

    feat: 改成溫度監控

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include <unistd.h>
+
+float readCpuTemp() {
+    std::ifstream file("/sys/class/thermal/thermal_zone0/temp");
+    int temp;
+    file >> temp;
+    return temp / 1000.0f;
+}
 
 int main() {
     while (true) {
-        std::cout << "Hello from my first service!" << std::endl;
+        float temp = readCpuTemp();
+        std::cout << "CPU Temperature: " << temp << " °C" << std::endl;
         sleep(3);
     }
     return 0;
